38b4c49af8c610314892b33929740ada2c1c26dd
Vocal Unrest <vocalunrest@icloud.com>
2024-10-23 11:04:13 -0400

    game state struct

--- game.ino
+++ game.ino
@@ -8,29 +8,32 @@ TinyScreen display = TinyScreen(TinyScreenPlus);
 #define SCREENWIDTH 94 
 #define SCREENHEIGHT 64
 
-enum State {
+enum Screen {
   //Tutorial,
   GAMEPLAY,
   END,
 };
 
-enum State state = GAMEPLAY; 
-int timerMax = 1500; // how long, in milliseconds, do we have to answer
-int timerStartMillis; // when did we start the timer
-int timerX = 0; // the last drawn X position of the timer
-int shapesHidden = false; // we hide the shapes halfway through the timer; have we done it yet?
+enum Screen screen = GAMEPLAY; 
+
+struct GameState {
+  int timerMax; // how long, in milliseconds, do we have to answer
+  int timerStartMillis; // when did we start the timer
+  int timerX; // the last drawn X position of the timer
+  bool shapesHidden; // we hide the shapes halfway through the timer; have we done it yet?
+  bool currSameShape; // are the currently generated shapes the same shape?
+  bool currSameColor;
+  int lives;
+  int correct;
+  int level;
+};
+
+struct GameState game = {.timerMax = 1500};
 
 int threshold = 15; // how many correct answers to progress to the next level
+int startingLives = 10;
 int levels = 2;
 char* prompts[] = {"Same color", "Same shape"};
-
-int correct = 0;
-int startingLives = 15;
-int lives;
-int level = 0; // increments to 1 by nextLevel()
-
-int currSameShape = false; // are the currently generated shapes the same shape?
-int currSameColor = false;
 
 // When button is clicked, becomes false until released
 bool clickable1 = true;
@@ -54,27 +57,27 @@ void setup() {
 
 void loop() {
   checkInput();
-  if (state == GAMEPLAY) {
+  if (screen == GAMEPLAY) {
      updateTimer();
   }
 }
 
 void checkInput() {
-  if (state == END && ((checkButton(TAButton2) && clickable2) || (checkButton(TAButton1) && clickable1))) {
-    level = 0;
+  if (screen == END && ((checkButton(TAButton2) && clickable2) || (checkButton(TAButton1) && clickable1))) {
+    game.level = 0;
     nextLevel();
   }
 
-  if (state == GAMEPLAY) {
+  if (screen == GAMEPLAY) {
     if (checkButton(TAButton2) && clickable2) {
-      if ((level == 1 && currSameColor) || (level == 2 && currSameShape)) {
+      if ((game.level == 1 && game.currSameColor) || (game.level == 2 && game.currSameShape)) {
          next(true);
        } else {
        next(false);
        }
     }
     else if (checkButton(TAButton1) && clickable1) {
-      if ((level == 1 && !currSameColor) || (level == 2 && !currSameShape)) {
+      if ((game.level == 1 && !game.currSameColor) || (game.level == 2 && !game.currSameShape)) {
         next(true);
       }
       else { next(false); }
@@ -94,66 +97,66 @@ void checkInput() {
 }
 
 void updateTimer() {
-  int progress = millis() - timerStartMillis;
-  int newTimerX = round(((double) progress / (double) timerMax) * SCREENWIDTH);
+  int progress = millis() - game.timerStartMillis;
+  int newTimerX = round(((double) progress / (double) game.timerMax) * SCREENWIDTH);
 
-  if (!shapesHidden && progress >= timerMax / 2) {
-    shapesHidden = true;
+  if (!game.shapesHidden && progress >= game.timerMax / 2) {
+    game.shapesHidden = true;
     display.clearWindow(0, 20, SCREENWIDTH, SCREENHEIGHT - 25); 
   }
-  if (progress >= timerMax) {
+  if (progress >= game.timerMax) {
     next(false);
     return;
   }
-  if (newTimerX != timerX) {
+  if (newTimerX != game.timerX) {
     display.drawLine(newTimerX,SCREENHEIGHT - 4,newTimerX,SCREENHEIGHT,TS_8b_Red);
   }
 }
 
 void next(bool wasCorrect) {
   if (wasCorrect) {
-    correct++;
+    game.correct++;
   } else {
-    lives--;
-    if (lives == 0) {
+    game.lives--;
+    if (game.lives == 0) {
       gameOver();
       return;
     }
   }
-  if (correct == threshold) {
+  if (game.correct == threshold) {
     nextLevel();
     return;
   }
-  timerStartMillis = millis(); 
+  game.timerStartMillis = millis(); 
   display.clearWindow(0, 20, SCREENWIDTH + 1, SCREENHEIGHT);
-  shapesHidden = false;
+  game.shapesHidden = false;
   drawHUD();
   drawShapes();
 }
 
 void nextLevel() {
-  if (level == levels) {
+  if (game.level == levels) {
     gameOver();
     return;
   }
-  state = GAMEPLAY;
-  level++;
-  lives = startingLives;
-  correct = 0;
-  currSameShape = false;
-  currSameColor = false;
-  timerStartMillis = millis(); 
+  screen = GAMEPLAY;
+  game.level++;
+  game.lives = startingLives;
+  game.correct = 0;
+  game.currSameShape = false;
+  game.currSameColor = false;
+  game.timerStartMillis = millis(); 
   display.clearWindow(0, 0, SCREENWIDTH + 1, SCREENHEIGHT);
-  shapesHidden = false;
+  game.shapesHidden = false;
   drawHUD();
   drawShapes();
 }
 
 void gameOver () {
-  state = END;
+  screen = END;
   display.clearWindow(0, 0, SCREENWIDTH + 1, SCREENHEIGHT);
   display.fontColor(TS_8b_White,TS_8b_Black);
-  if (lives == 0) {
+  if (game.lives == 0) {
     char* txt = "Game Over";
     display.setCursor(SCREENWIDTH / 2 - display.getPrintWidth(txt) / 2, 10);
     display.print(txt);
@@ -162,7 +165,7 @@ void gameOver () {
     display.setCursor(SCREENWIDTH / 2 - width / 2, 25);
     display.print(txt);
     display.setCursor(SCREENWIDTH / 2 + display.getPrintWidth(txt) / 2, 25);
-    display.print(level);
+    display.print(game.level);
     return;
   }
   char* txt = "You win";
@@ -175,50 +178,50 @@ void drawHUD () {
   
   display.fontColor(TS_8b_Green,TS_8b_Black);
   display.setCursor(2, 6);
-  display.print(correct);
+  display.print(game.correct);
 
   display.fontColor(TS_8b_White,TS_8b_Black);
-  display.setCursor(SCREENWIDTH / 2 - display.getPrintWidth(prompts[level - 1]) / 2, 6);
-  display.print(prompts[level - 1]);
+  display.setCursor(SCREENWIDTH / 2 - display.getPrintWidth(prompts[game.level - 1]) / 2, 6);
+  display.print(prompts[game.level - 1]);
   
   display.fontColor(TS_8b_Red,TS_8b_Black);
   display.setCursor(SCREENWIDTH - 13, 6);
-  display.print(lives);
+  display.print(game.lives);
 }
 
 void drawShapes() {
-  if (level == 1) {
+  if (game.level == 1) {
     // We want to get two different colors half the time (shape is totally random)
     int leftColorIndex = random(3);
     int rightColorIndex;
     if (random(2) == 0) {
       rightColorIndex = leftColorIndex;
-      currSameColor = true;
+      game.currSameColor = true;
     } else {
       // choose the other color and make sure it's different
       rightColorIndex = random(2);
       if (rightColorIndex >= leftColorIndex) {
         rightColorIndex++;
       }
-      currSameColor = false;
+      game.currSameColor = false;
     }
 
     drawShape(true, colors[leftColorIndex], random(3));
     drawShape(false, colors[rightColorIndex], random(3));
   }
-  if (level == 2) {
+  if (game.level == 2) {
     // We want to get two different shapes half the time (color is totally random)
     int leftShapeType = random(3);
     int rightShapeType;
     if (random(2) == 0) {
       rightShapeType = leftShapeType;
-      currSameShape = true;
+      game.currSameShape = true;
     } else {
       rightShapeType = random(2);
       if (rightShapeType >= leftShapeType) {
         rightShapeType++;
       }
-      currSameShape = false;
+      game.currSameShape = false;
     }
 
     drawShape(true, colors[random(3)], leftShapeType);
